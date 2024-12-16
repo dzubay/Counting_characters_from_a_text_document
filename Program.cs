f26b8cef02dbcbcab7eab7ef6de8266526ef010c
dzubay <dzubay22112211@yandex.ru>
2024-12-16 21:11:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Test
+{
+    public static void Main()
+    {
+        // Имя файла
+        string filename = "note1.txt";
+
+        // Символ, который нужно подсчитать
+        char symbol = 'A';
+
+        // Считываем текст из файла
+        string[] lines = File.ReadAllLines(filename);
+
+        // Подсчитываем количество заданного символа
+        int count = 0;
+        foreach (string line in lines)
+        {
+            foreach (char c in line)
+            {
+                if (c == symbol)
+                {
+                    count++;
+                }
+            }
+        }
+
+        // Выводим результат
+        Console.WriteLine($"Количество символов '{symbol}': {count}");
+
+    }
+}
